1c55f048a472f25d8e59d5cead1d46167adabc96
EdwardGoomba <edward@dock90.io>
2020-06-17 16:45:09 -0400

    Install gatsby widget, setu pdashboard

---
+++ dashboardConfig.js
@@ -0,0 +1,20 @@
+export default {
+  widgets: [
+    {
+      name: 'gatsby',
+      options: {
+        sites: [
+          {
+            siteUrl: 'https://callalootrinidadiankitchen.netlify.app',
+          }
+        ]
+      }
+    },
+    {
+      name: 'project-info'
+    },
+    {
+      name: 'project-users'
+    }
+  ]
+}
